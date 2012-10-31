818d9f9eefc49c80f4adfd2ce65b3197d9fe263d
Ram Ranganathan <ramr@redhat.com>
2012-10-31 13:15:20 -0700

    Fixup pacman application for typeless gear changes

--- server.js
+++ server.js
@@ -111,10 +111,10 @@ process.on('exit', function() { terminator(); });
    pacmanController = false;
    io = require('socket.io');
    mongo = require('mongodb');
-   murl = "mongodb://" + process.env.OPENSHIFT_NOSQL_DB_USERNAME +
-          ":" + process.env.OPENSHIFT_NOSQL_DB_PASSWORD + "@" + 
-          process.env.OPENSHIFT_NOSQL_DB_HOST + ":" +
-          process.env.OPENSHIFT_NOSQL_DB_PORT + "/" +
+   murl = "mongodb://" + process.env.OPENSHIFT_MONGODB_DB_USERNAME +
+          ":" + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" + 
+          process.env.OPENSHIFT_MONGODB_DB_HOST + ":" +
+          process.env.OPENSHIFT_MONGODB_DB_PORT + "/" +
           process.env.OPENSHIFT_APP_NAME;
    mongo.connect(murl, function(err, conn) {
       conn.on('error', function(err) {
